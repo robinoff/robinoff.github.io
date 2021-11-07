ee268d8f5544dc33259b97dbf4259e7cdf1312dc
Robinoff <robinoff@tlen.pl>
2021-11-07 08:42:42 +0100

    init

--- odliczanie/main.js
+++ odliczanie/main.js
@@ -1,29 +1,43 @@
-const endTime = new Date('2021-11-06 17:00:00').getTime();
+function counter() {
 
-const spanD = document.querySelector('span.d');
-const spanH = document.querySelector('span.h');
-const spanM = document.querySelector('span.m');
-const spanS = document.querySelector('span.s');
+    const endTime = new Date('2021-11-07 8:42:00').getTime();
 
-setInterval(() => {
- const nowTime = new Date().getTime();
- // const time = Math.floor((endTime - nowTime) / 1000);
- const time = endTime - nowTime;
- const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
- console.log(days);
+    const spanD = document.querySelector('span.d');
+    const spanH = document.querySelector('span.h');
+    const spanM = document.querySelector('span.m');
+    const spanS = document.querySelector('span.s');
 
- let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
- // Przykład - dodanie 0 przeg godziną 
- hours = hours < 10 ? `0${hours}` : hours;
+    
+    const interval = setInterval(() => {
+        const nowTime = new Date().getTime();
+        // const time = Math.floor((endTime - nowTime) / 1000);
+        const time = endTime - nowTime;
+        const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+        
 
- let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
- minutes = minutes < 10 ? `0${minutes}` : minutes;
+        let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
 
- let secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
- secs = secs < 10 ? `0${secs}` : secs;
+        hours = hours < 10 ? `0${hours}` : hours;
 
- spanD.textContent = days;
- spanH.textContent = hours;
- spanM.textContent = minutes;
- spanS.textContent = secs;
-}, 1000)
+        let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        
+        let secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+        secs = secs < 10 ? `0${secs}` : secs;
+
+        spanD.textContent = `d:${days}`;
+        spanH.textContent = `h:${hours}`;
+        spanM.textContent = `m:${minutes}`;
+        spanS.textContent = `s:${secs}`;
+        
+        
+        if (spanD.textContent === 'd:0' && spanH.textContent === 'h:00' && spanM.textContent === 'm:00' && spanS.textContent === 's:00') {
+            clearInterval(interval);
+            console.log('teraz');
+        }
+    }, 1000);
+
+
+}
+
+counter()
